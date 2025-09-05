990ca879e1a851bb23c9389bf3c839bbdeb957b0
KaTo3107 <mail@kato3107.de>
2025-09-05 10:06:33 +0200

    Create Microsoft.Powershell_ProfileForWork.ps1

---
+++ Oh My Posh/Microsoft.Powershell_ProfileForWork.ps1
@@ -0,0 +1,6 @@
+oh-my-posh init pwsh --config $env:POSH_THEMES_PATH/kato-paradox.omp.json | Invoke-Expression
+Set-Location C:\Entwicklung
+
+Clear-Host
+echo $env:computername": Machine is Ready to Start"
+echo "------------------------------------------------"
