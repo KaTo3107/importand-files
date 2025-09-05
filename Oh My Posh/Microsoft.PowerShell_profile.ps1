e847a59de49d2a7bce07e552030851f3c647c9f0
KaTo3107 <mail@kato3107.de>
2025-09-05 10:04:26 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Oh My Posh/Microsoft.PowerShell_profile.ps1
+++ Oh My Posh/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,5 @@
 oh-my-posh init pwsh --config $env:POSH_THEMES_PATH/kato-paradox.omp.json | Invoke-Expression
+
+Clear-Host
+echo "IUCON-NB-028: Machine is Ready to Start"
+echo "------------------------------------------------"
